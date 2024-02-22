cab7ca96ea018bfc665a3401e499f255d7125a49
Elvira55872 <elli-x1996@mail.ru>
2024-02-22 14:56:42 +0100

    дз 28

---
+++ basic_programming/lesson_28_homework/src/Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    Node next;   // указатель на следующий элемент
+    int data;   // данные
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
