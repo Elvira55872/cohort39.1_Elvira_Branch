fea53532bbb32480e116bbc6339889822a3481ef
Elvira55872 <elli-x1996@mail.ru>
2024-03-02 18:10:20 +0100

    .

--- basic_programming/lesson_30_homework/src/Main.java
+++ basic_programming/lesson_30_homework/src/Main.java
@@ -1,38 +1,49 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Задача 1.
  * Работа с Map
- *
+ * <p>
  * Реализовать метод removeSameValues(Map<K, V> map).
  * Данный метод должен принимать в качестве аргумента мапу,
  * ваша задача - Оставить в данной мапе исключительно пары с уникальным значением (не ключом).
  * null - также может быть представлен, в этом случае допустимо оставить только одно null значение в мапе.
- *
+ * <p>
  * Упрощенная версия: использовать данный метод на основе removeSameValues(Map<String, String> map)
  */
-public class Main<K, V> {
+
+/**
+ * HINTS
+ * 1. Создаем временный набор (Set) для хранения уникальных значений, которые мы уже добавили в итоговую мапу.
+ * Это позволит нам проверять, существует ли значение в мапе без необходимости итерировать всю мапу каждый раз.
+ * <p>
+ * 2. Итерируем исходную мапу, чтобы проверить каждое значение. Для каждого значения мы проверяем,
+ * содержится ли оно уже в нашем временном наборе уникальных значений.
+ * <p>
+ * 3. Если значение уже содержится в наборе, удаляем соответствующий ключ из исходной мапы.
+ * Если значение не содержится в наборе, добавляем его в набор уникальных значений.
+ */
+public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> m = new HashMap<>();
-        m.put(0,"lala");
-        m.put(1,"baba");
-        m.put(2,"lala");
-        m.put(3,"nana");
-
-        System.out.println(m.values());
-
+        Map<String, String> m = new HashMap<>();
+        m.put("null", "lala");
+        m.put("one", "baba");
+        m.put("two", "lala");
+        m.put("three", "nana");
+        System.out.println(m.entrySet());
+        System.out.println(removeSameValues(m));
     }
 
-    public void removeSameValues(Map<K, V> map){
-        // метод .containsValue() - проверяет, что данное значение есть в мапе
-        for (int i = 0; i < map.size(); i++) {
-            ArrayList<V> values = new ArrayList<>(map.values());
-            for (V data: values){
+    public static Map<String, String> removeSameValues(Map<String, String> map) {
+        // по подсказкам Ильяса
+        Map<String, String> result = new HashMap<>(); // новая пустая мапа куда будем класть уникальные значения
+        Set<Map.Entry<String, String>> tempSet = map.entrySet(); // сет только с уникальными значениями из мапы
 
+        for (Map.Entry<String, String> pair : tempSet) {
+            if (!result.containsValue(pair.getValue())) {
+                result.put(pair.getKey(), pair.getValue());
             }
-
         }
+        return result;
     }
 }
